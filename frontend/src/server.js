0316c6f4759ae52f8149bfab01e1f15688bc79bd
Jeff Jiang <jeff.jiang13@gmail.com>
2023-05-14 22:29:58 -0400

    first commit

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://ecommerce-5-backend.onrender.com/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://ecommerce-5-backend.onrender.com/";
