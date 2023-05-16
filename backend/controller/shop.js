282c4b26a2593e0ded80731f365fcf35e7f10dcd
Jeff Jiang <jeff.jiang13@gmail.com>
2023-05-15 23:41:47 -0400

    first commit

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -171,7 +171,7 @@ router.get(
   "/logout",
   catchAsyncErrors(async (req, res, next) => {
     try {
-      res.clearCookie("seller_token", "", {
+      res.clearCookie("seller_token", {
         expires: new Date(Date.now()), // set the cookie expiration date to now
         httpOnly: true,
         sameSite: "none",
