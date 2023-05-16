3fa17cf00e0fdab2819f3259daf90007eb04d1e7
Jeff Jiang <jeff.jiang13@gmail.com>
2023-05-16 16:24:58 -0400

    login error

--- backend/utils/shopToken.js
+++ backend/utils/shopToken.js
@@ -7,8 +7,10 @@ const sendShopToken = (user, statusCode, res) => {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     sameSite: "none",
-    secure: process.env.NODE_ENV === 'PRODUCTION' ? true : false,
-    domain: 'jj-ecommerce-5.vercel.app',
+    secure: true,
+    domain: 'https://jj-ecommerce-5.vercel.app', // Add this line
+    secure: process.env.NODE_ENV === 'PRODUCTION' ? true : false, // Modify this line
+
   };
 
   res.status(statusCode).cookie("seller_token", token, options).json({
